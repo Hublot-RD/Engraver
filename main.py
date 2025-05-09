810807484df2cbdc0002065641469f0341b56e68
vphilippoz <82140967+vphilippoz@users.noreply.github.com>
2025-05-09 08:35:28 +0200

    Working version for "raw" and smoothed path (ugly)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from math import pi
+import numpy as np
+from PIL import Image
 import warnings
-from builder3d import export_path_to_csv
+# from builder3d import export_path_to_csv
 from audio_processor import mp3_to_amplitude_series, apply_low_pass_filter
 from parameters import default_parameters as p
 
@@ -11,32 +13,39 @@ if p.filter_active:
     # Apply low pass filter
     amplitudes, frame_rate = apply_low_pass_filter(amplitudes, frame_rate, cutoff_freq=p.cutoff_freq, downsample=True)
 
-# Create path points from amplitudes
-path_points_cyl = []
-path_points_plane = []
-for i,amp in enumerate(amplitudes):
-    radius = p.R-p.depth
-    phase = (i) * p.speed_angular/frame_rate
-    elevation = phase*p.pitch/(2*pi) + amp*p.max_amplitude/2 + p.end_margin + p.start_pos
+# Create blank image
+img_height, img_width = int(p.L / p.pixel_size), int(2 * p.R * np.pi / p.pixel_size)
+image = p.white * np.ones((img_height, img_width), dtype=np.uint8)
 
-    x = p.R
-    y = radius * phase
-    z = elevation
-    if elevation > p.L - p.end_margin:
+# Create path points from amplitudes
+for i,amp in enumerate(amplitudes):
+    phase = (i) * p.speed_angular/frame_rate
+
+    x = p.R * phase
+    y = phase*p.pitch/(2*pi) + amp*p.max_amplitude/2 + p.end_margin + p.start_pos
+    if y > p.L - p.end_margin:
         warnings.warn(f"Engraving stopped by end of cylinder.")
         break
     else:
-        path_points_cyl.append((radius, phase, elevation))
-        path_points_plane.append((x, y, z))
+        # Color the corresponding pixel in the image according to the distance to the center of the engraving
+        x_pixel, y_pixel = int(x / p.pixel_size) % img_width, int(y / p.pixel_size)
+        for j in range(p.engraving_pixel_width):
+            dy = j - p.engraving_pixel_width//2
+            if 0 <= y_pixel+dy < img_height:
+                y_pixel_center = (y_pixel+dy + 0.5) * p.pixel_size
+                distance = abs(y - y_pixel_center)
+                image[y_pixel+dy, x_pixel] = int(min(p.white * distance/(p.width/2), p.white))
+        # image[y_pixel-2:y_pixel+3, x_pixel] = 255*2/3
+        # image[y_pixel-1:y_pixel+2, x_pixel] = 255*1/3
+        # image[y_pixel, x_pixel] = 0
 
 print(f"Path contains {i+1}/{len(amplitudes)} points ({round((i+1)/len(amplitudes)*100,3)} %) from the audio segment.")
-used_length = path_points_cyl[-1][-1] - p.start_pos - p.end_margin
-print(f"Engraving takes {round(used_length, 3)} mm, {round(used_length/(p.L - 2*p.end_margin)*100, 3)} % of the available space of the cylinder.")
+# used_length = path_points_plane[-1][-1] - p.start_pos - p.end_margin
+# print(f"Engraving takes {round(used_length, 3)} mm, {round(used_length/(p.L - 2*p.end_margin)*100, 3)} % of the available space of the cylinder.")
 
-# Create the engraved cylinder and wire
-export_path_to_csv(path_points_cyl, p.output_folder+p.output_filename+'_cyl', split_files=p.split_files, files_per_turn=p.files_per_turn, cyl_coord=True)
-export_path_to_csv(path_points_plane, p.output_folder+p.output_filename+'_plan', split_files=p.split_files, files_per_turn=p.files_per_turn, cyl_coord=False)
+# Save the image
+Image.fromarray(image).save(p.output_filename+".tiff", format="TIFF", quality=100, subsampling=0)
 
 # Export parameters to a text file
-with open(p.output_folder+p.output_filename+"_parameters.txt", 'w') as f:
-    f.write(str(p))
+# with open(p.output_folder+p.output_filename+"_parameters.txt", 'w') as f:
+#     f.write(str(p))
--- parameters.py
+++ parameters.py
@@ -1,4 +1,4 @@
-from math import pi
+from math import pi, tan, radians
 from datetime import date
 
 class ParameterSet:
@@ -7,8 +7,9 @@ class ParameterSet:
     L = 125.0  # Length [mm]
 
     # Engraving
-    depth = 0  # Depth of the cut [mm]
+    depth = 0.025  # Depth of the cut [mm]
     angle = 90.0  # Angle of the cut [°]
+    width = 2 * depth * tan(radians(angle/2))  # Width of the cut [mm]
     pitch = 1 # Pitch of the spiral [mm]
     max_amplitude = 0.025 # Maximal amplitude of the engraved audio signal (peak-peak) [mm]
     speed = 33.5*pi/30*150/2 # Longitudinal reading speed of a 12" vinyl at the inner edge [mm/s]
@@ -19,14 +20,20 @@ class ParameterSet:
     files_per_turn = 2 # Number of files per turn of the cylinder
 
     # Audio
-    filter_active = True
+    filter_active = False
     cutoff_freq = 5000 # Hz
     start_time = 0 # How many seconds to crop from the start of the audio
     duration = 5 # Duration of the audio signal [s]
 
+    # Image
+    pixel_size = 0.01 # Size of a pixel in the image [mm]
+    engraving_pixel_width = int(width / pixel_size) + 2 # Width of the engraving in pixels with a margin of 1 pixel on each side
+    white = 255 # Color for the engraving
+    black = 0 # Color for the engraving
+
     # Folders and file name
     input_folder = "./audio_files/"
-    input_filename = "200Hz.mp3"
+    input_filename = "1000Hz.mp3"
     output_folder = "./3d_files/"
     output_filename = f'{round(depth*1e3)}_{round(max_amplitude*1e3)}_{round(pitch*1e3)}_{input_filename.split(".")[0]}_path'
     
